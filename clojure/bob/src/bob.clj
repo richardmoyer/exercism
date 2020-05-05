20f274a038b461248e7bae0d0b6f559333d1bc01
richardmoyer <richmoyer@gmail.com>
2020-05-04 22:29:12 -0500

    bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,46 @@
-(ns bob)
+(ns bob (:require [clojure.string :as st]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def responses
+  {:yelling "Whoa chill out!"
+   :question "Sure."
+   :loud-question "Calm down, I know what I'm doing"
+   :blank "Fine. Be that way"
+   :default "Whatever"})
+
+(defn- yelling? [s]
+  (and (some #(Character/isLetter %) s)
+       (= s (st/upper-case s))))
+
+(defn- question? [s]
+  (= \? (last (st/trimr s))))
+
+(defn response-for [s]
+  ())
+(ns bob
+  (:require [clojure.string :as st]))
+
+;; (def responses
+;;   {:yelling       "Whoa, chill out!"
+;;    :question      "Sure."
+;;    :loud-question "Calm down, I know what I'm doing!"
+;;    :blank         "Fine. Be that way!"
+;;    :default       "Whatever."})
+
+;; (defn- yelling? [s]
+;;   (and (some #(Character/isLetter %) s)
+;;        (= s (st/upper-case s))))
+
+;; (defn- question? [s]
+;;   (= \? (last (st/trimr s))))
+
+;; (defn respond [s]
+;;   (if (st/blank? s)
+;;     :blank
+;;     (let [q? (question? s)
+;;           y? (yelling? s)]
+;;       (cond q? (if y? :loud-question :question)
+;;             y? :yelling
+;;             :else :default))))
+
+;; (defn response-for [s]
+;;   (responses (respond s)))
