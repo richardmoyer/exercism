bf90516c5d9fff53cce86af1bb190e0d2af13c1f
richm <richmoyer@gmail.com>
2020-05-06 20:27:26 -0500

    bob solution

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -14,33 +14,16 @@
 (defn- question? [s]
   (= \? (last (st/trimr s))))
 
+(defn respond [s]
+  (if (st/blank? s)
+    :blank
+    (let [q? (question? s)
+          y? (yelling? s)]
+      (cond q? (if y? :loud-question :question)
+            y? :yelling
+            :else :default))))
+
 (defn response-for [s]
-  ())
-(ns bob
-  (:require [clojure.string :as st]))
+  (responses (respond s)))
 
-;; (def responses
-;;   {:yelling       "Whoa, chill out!"
-;;    :question      "Sure."
-;;    :loud-question "Calm down, I know what I'm doing!"
-;;    :blank         "Fine. Be that way!"
-;;    :default       "Whatever."})
-
-;; (defn- yelling? [s]
-;;   (and (some #(Character/isLetter %) s)
-;;        (= s (st/upper-case s))))
-
-;; (defn- question? [s]
-;;   (= \? (last (st/trimr s))))
-
-;; (defn respond [s]
-;;   (if (st/blank? s)
-;;     :blank
-;;     (let [q? (question? s)
-;;           y? (yelling? s)]
-;;       (cond q? (if y? :loud-question :question)
-;;             y? :yelling
-;;             :else :default))))
-
-;; (defn response-for [s]
-;;   (responses (respond s)))
+(response-for "WHAT IS UP?")
